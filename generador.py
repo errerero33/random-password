630254d630a32676184f845f36102b1b76ed6794
errerero33 <75173531+errerero33@users.noreply.github.com>
2021-06-21 16:23:11 +0200

    Update generador.py

--- generador.py
+++ generador.py
@@ -1,8 +1,24 @@
-from random import choice
+import subprocessfrom VALID import ns, OKI, opt
+import random
+import string 
+import webbrowser as wb
 
-longitud = int(raw_input('Longitud del pass: '))
-valores = “0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#%&+”
-p = “”
-p = p.join([choice(valores) for i in range(longitud)])
-print p
-raw_input()
+while True:
+    print('****GENERADOR DE CONTRASEÑAS****')
+    minus=OKI(input('Indique número mínimo de minusculas:'))
+    mayus=OKI(input('Indique número mínimo de mayusculas:'))
+    numeros=OKI(input('Indique número mínimo de numeros:'))
+    longitud=OKI(input('Indique longitud de la contraseña:'))
+    caract=string.ascii_letters+string.digits
+    while True 
+    contraseña=('').join(random.choice(caract)for i in range(longitud))
+    print(contraseña)
+    if(sum(c.islower() for c in contraseña)>=minus
+       and sum(c.isupper() for c in contraseña)>=mayus
+       and sum(c.isupper() for c in contraseña)>=numeros):
+       break
+    print('')
+    print('su contraseña: ',contraseña)
+    print('recuerda seguirme')
+
+wb.open('https://twitch.tv/golduck06')
